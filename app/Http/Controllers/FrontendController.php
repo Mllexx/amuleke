d21e72810fd9ec5b2e24f3068af08e598780122c
Andrew, Muleke <mulekea3@gmail.com>
2023-01-23 11:29:13 +0300

    add function to pull articles from Medium

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,23 +9,39 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        $client = new Client();
-        /*
-        $response = $client->request('GET', 'https://api.medium.com/v1/users/1cdea0d2f90e3d15b47ff58ff08d958ee7cb25e8013d48b9443099033729c0e9a/publications', [
-            'headers' => [
-                'Authorization' => 'Bearer 21a4e60ac1d02d5c7ac87fa41f0f2d002b0dd8b53dc9c3455e576c31eff2d6dc2',
-                'Accept' => 'application/json',
-            ],
-        ]);
+        $medium_posts = $this->fetchMedium();
+        dd($medium_posts);
+        return view('medium', ['publications' => $medium_posts]);
+        ##return view('home');
+    }
 
-        $publications = json_decode($response->getBody(), true);
-        */
-        $xml = simplexml_load_file('https://amuleke.medium.com/feed');
-        $json = json_encode($xml);
-        $publications = json_decode($json, true);
-        dd($publications);
-        ## return view('medium', ['publications' => $publications]);
-        return view('home');
+    /**
+     * Fetch posts from medium account in JSON format
+     * @return Object
+     */
+    function fetchMedium()
+    {
+        $medium_username = env('MEDIUM_USERNAME');
+        $client = new Client();
+
+        try {
+            $res = $res = $client->get("https://medium.com/@$medium_username?format=json");
+        } catch (\Exception $exception) {
+            return false;
+        }
+
+        if (!$res->getStatusCode() == 200) {
+            return false;
+        }
+
+        $body = json_decode(str_replace('])}while(1);</x>', '', $res->getBody()));
+
+        if (!isset($body->success, $body->payload)) {
+            return false;
+        }
+
+        $posts = $body->payload->references->Post;
+        return $posts;
     }
 
 }
