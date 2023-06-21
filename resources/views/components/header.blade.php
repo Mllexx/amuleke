20e1e49cc538895147617525265ceb5c77d4f630
mllexx <mulekea3@gmail.com>
2023-06-21 19:06:16 +0300

    Add analytics tag

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,3 +28,12 @@
         </ul>
     </nav>
 </header>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PPTV9EBVY4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PPTV9EBVY4');
+</script>
