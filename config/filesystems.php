dd0c2753fcf5b55ce7c15085b937ed5717687a25
mllexx <mulekea3@gmail.com>
2023-06-21 09:47:07 +0300

    Update db backups logic and storage path

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'db-backup' => [
+            'driver' => 'local',
+            'root' => storage_path('db'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
